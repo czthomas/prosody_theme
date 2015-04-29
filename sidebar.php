767e38d4f7df4e5fd1628e5bc86d472569fb1ac2
Scott Bailey <csb5t@virginia.edu>
2015-04-29 09:10:31 -0400

    Sketches out way forward in create necessary data fields for the various sorting loops

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,7 @@
 <div id="sidebar">
     <h3>List of Poems</h3>
     <div id="poem-sorting">
+    <!-- Below, the first loop uses a default part of the post object. The next three loops should probably be done using meta data. Since they are all basically the same, we could try creating a function that would just be passed a meta key to get the data to be sorted. Trick might be what is displayed. Could pass a template tag if that is sufficient.-->
         <h4 class="poem-sort-method">By Title</h4>
         <!-- By Title will need to get all poems and sort by ascending order -->
             <div class="poem-results">
@@ -20,9 +21,9 @@
                 </ul>
             </div>
         <h4 class="poem-sort-method">By Difficulty</h4>
-        <!-- By Difficulty will require that all poems have a difficulty assigned by category. The query will then sort by category, displaying in a particular order -->
+        <!-- By Difficulty will require that all poems have a difficulty assigned in meta data. Will then group and sort by difficulty. -->
         <h4 class="poem-sort-method">By Type</h4>
-        <!-- Type will also require assignment by category and then sorting by category -->
+        <!-- Type will also require assignment by meta data and then sorting by type. -->
         <h4 class="poem-sort-method">By Author</h4>
         <!-- Author will require creating an author meta box for each post, getting that data, and then sorting by meta_value -->
     </div>
