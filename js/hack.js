33716f876be18edb7b9b0d580ed42557e67eb160
Scott Bailey <csb5t@virginia.edu>
2015-06-29 14:58:12 -0400

    Add in a view for poem resources

--- js/hack.js
+++ js/hack.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
 
     var contentWidth = $('.poem-home').width();
     $('#utils').width(contentWidth + 10);
+
+    $('#poem_resources').hide();
+    $('#poem_text_tab').click(function(e){
+        e.preventDefault();
+        $('#poem_resources').hide();
+        $('#poem_text').show();
+    });
+    $('#poem_resources_tab').click(function(e){
+        e.preventDefault();
+        $('#poem_text').hide();
+        $('#poem_resources').show();
+    });
+
 });
