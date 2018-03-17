39275bba0061490e08912f56ac5acf0bdf16184b
Christopher Thomas <acheetahwhipsmothsmrtrot@gmail.com>
2018-03-17 17:07:43 -0400

    Added style for WP pages

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
 <div id="main">
 <div class="container">
 <div class="row">
-<div class="content col-lg-8 col-md-8 col-sm-8">
+<div class="content post-home col-lg-9 col-md-9 col-sm-12">
 
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
@@ -14,15 +14,9 @@
     <?php endwhile; endif; ?>
 </div><!-- ends .content -->
 
-<div class="col-lg-4 col-md-4 col-sm-4">
-    <?php get_sidebar(); ?>
-</div>
-
 </div><!-- ends row -->
 </div><!-- ends .container -->
 
 </div><!-- ends .main -->
 
 <?php get_footer(); ?>
-
-
